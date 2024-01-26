ef5dc03145549e73698b2366f55942be6f49586a
Liqun Li <liqli@microsoft.com>
2024-01-26 18:23:30 +0800

    fix issues

--- project/plugins/document_retriever.py
+++ project/plugins/document_retriever.py
@@ -3,22 +3,34 @@ from taskweaver.plugin import Plugin, register_plugin
 
 @register_plugin
 class DocumentRetriever(Plugin):
-    def __call__(self, query: str, size: int = 3):
+    vectorstore = None
+
+    def _init(self):
         try:
             from langchain_community.embeddings import HuggingFaceEmbeddings
             from langchain_community.vectorstores import FAISS
         except ImportError:
             raise ImportError("Please install langchain-community first.")
 
-        embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-        vectorstore = FAISS.load_local(
+        self.embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        self.vectorstore = FAISS.load_local(
             folder_path=self.config.get("index_folder"),
-            embeddings=embeddings,
+            embeddings=self.embeddings,
         )
 
-        result = vectorstore.similarity_search(
+    def __call__(self, query: str, size: int = 5):
+        if self.vectorstore is None:
+            self._init()
+
+        result = self.vectorstore.similarity_search(
             query=query,
             k=size,
         )
 
-        return [{"chunk": r.page_content, "metadata": r.metadata} for r in result]
+        return [
+            {
+                "chunk": r.page_content,
+                "metadata": r.metadata,
+            }
+            for r in result
+        ]
