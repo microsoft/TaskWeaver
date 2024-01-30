e299e75251e17639da9d1fe7a7cd65155487515f
Schroedinger's Cat <32928431+zhangxu0307@users.noreply.github.com>
2024-01-30 14:14:05 +0800

    Xz/customized llm api (#173)
    - add docs about how to implement your own LLM API
    - add LLM API test script

---
+++ scripts/llm_api_test.py
@@ -0,0 +1,48 @@
+import argparse
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+
+from injector import Injector
+
+from taskweaver.config.config_mgt import AppConfigSource
+from taskweaver.llm import LLMApi, format_chat_message
+from taskweaver.logging import LoggingModule
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--project_dir",
+    type=str,
+    default=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "project"),
+)
+
+parser.add_argument("--query", type=str, default="Hello!")
+
+args = parser.parse_args()
+
+
+def LLM_API_test():
+    app_injector = Injector([LoggingModule])
+    app_config = AppConfigSource(
+        config_file_path=os.path.join(
+            args.project_dir,
+            "taskweaver_config.json",
+        ),
+    )
+    app_injector.binder.bind(AppConfigSource, to=app_config)
+    llm_api = app_injector.create_object(LLMApi)
+
+    llm_stream = llm_api.chat_completion_stream(
+        messages=[format_chat_message(role="user", message=args.query)],
+        use_backup_engine=False,
+        use_smoother=True,
+    )
+
+    for msg in llm_stream:
+        print(msg["content"], end="")
+
+
+if __name__ == "__main__":
+    LLM_API_test()
